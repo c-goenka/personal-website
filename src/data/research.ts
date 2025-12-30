beacec4ec0d2ece581e55e1cf7b38d0a44d34a38
chetan <cgoenka30@gmail.com>
2025-12-30 12:07:46 -0800

    update research order to sort by relevance

--- src/data/research.ts
+++ src/data/research.ts
@@ -25,6 +25,17 @@ export const researchProjects: ResearchProject[] = [
         links: {}
     },
     {
+        id: "code-explanations",
+        title: "Code Explanations: Automated Hierarchical Descriptions of Program Behavior",
+        description: "This project introduces a tool that uses AI to generate layered code explanations—per-line, per-block, and by function parameter—to help users better understand and modify programs. Built with CodeMirror and GPT-4o, the system overlays interactive insights directly onto code. A pilot study showed its potential, and future work aims to expand its usability across skill levels, tasks, and languages.",
+        // status: "submitted",
+        venue: "CHI 2025 Workshop Submission (Unpublished)",
+        links: {
+            pdf: "/papers/code-explanations-chi25.pdf",
+            code: "https://github.com/c-goenka/code-explanations"
+        }
+    },
+    {
         id: "memory-bottle",
         title: "Memory Bottle",
         description: "Memory Bottle is an interactive vessel that lets users preserve and relive fleeting moments by recording sound and color. Powered by an ESP32 and Arduino ecosystem, the system uses a state machine to manage sensor data and wireless uploads. Users simply tilt the bottle to 'pour' the memory out, triggering an immersive playback where the audio is heard and the captured color fills the room.",
@@ -37,14 +48,15 @@ export const researchProjects: ResearchProject[] = [
         },
     },
     {
-        id: "code-explanations",
-        title: "Code Explanations: Automated Hierarchical Descriptions of Program Behavior",
-        description: "This project introduces a tool that uses AI to generate layered code explanations—per-line, per-block, and by function parameter—to help users better understand and modify programs. Built with CodeMirror and GPT-4o, the system overlays interactive insights directly onto code. A pilot study showed its potential, and future work aims to expand its usability across skill levels, tasks, and languages.",
-        // status: "submitted",
-        venue: "CHI 2025 Workshop Submission (Unpublished)",
+        id: "bees-research",
+        title: "A Quantitative Analysis of a Summer Bridge Program's Impact on Students' Non-Academic Indicators",
+        description: "Evaluation a summer bridge program for underserved Computing students, measuring its impact on non-academic factors like sense of belonging, resource awareness, and science identity. The research found that both online and in-person formats improved outcomes, with in-person programs showing stronger effects, especially on social confidence.",
+        status: "published",
+        venue: "Frontiers in Education (FIE)",
+        year: "2024",
         links: {
-            pdf: "/papers/code-explanations-chi25.pdf",
-            code: "https://github.com/c-goenka/code-explanations"
+            pdf: "/papers/bees-fie2024.pdf",
+            // code: "https://github.com/username/lorem-ipsum"
         }
     },
     {
@@ -55,18 +67,6 @@ export const researchProjects: ResearchProject[] = [
         venue: "Jacobs Design Showcase 2024",
         links: {
             poster: "/images/pail-poster.png",
-        }
-    },
-    {
-        id: "bees-research",
-        title: "A Quantitative Analysis of a Summer Bridge Program's Impact on Students' Non-Academic Indicators",
-        description: "Evaluation a summer bridge program for underserved Computing students, measuring its impact on non-academic factors like sense of belonging, resource awareness, and science identity. The research found that both online and in-person formats improved outcomes, with in-person programs showing stronger effects, especially on social confidence.",
-        status: "published",
-        venue: "Frontiers in Education (FIE)",
-        year: "2024",
-        links: {
-            pdf: "/papers/bees-fie2024.pdf",
-            // code: "https://github.com/username/lorem-ipsum"
         }
     },
 ];
