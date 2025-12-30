37ac5383175e14cb9e4e19a467ceab59397eec21
chetan <cgoenka30@gmail.com>
2025-12-29 22:13:25 -0800

    update default theme to match system

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -9,7 +9,7 @@ export function ThemeProvider({
     return (
         <NextThemesProvider
             attribute="data-theme"
-            defaultTheme="light"
+            defaultTheme="system"
             enableSystem={true}
             disableTransitionOnChange={false}
             {...props}
